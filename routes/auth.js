c28a600046cc47a93bbee21cb0203ea79fa9c6bf
Rowan Williams <rmtwilliams@hotmail.co.uk>
2021-03-08 16:49:10 +0100

    improved log in

--- routes/auth.js
+++ routes/auth.js
@@ -40,20 +40,23 @@ router.post("/signin", async (req, res, next)=>{
     const foundUser = await UserModel.findOne({ email: email });
   
         if (!foundUser) {
-        req.flash("error", "Invalid credentials");
-        res.redirect("/signin");
+            console.log("wrong email");
+        // req.flash("error", "Invalid credentials");
+        res.redirect("/auth/signin");
         } else {
         const isSamePassword = bcrypt.compareSync(password, foundUser.password);
         
                 if (!isSamePassword) {
-                    req.flash("error", "Invalid credentials");
-                    res.redirect("/signin");
+                    console.log("wrong password");
+                    // req.flash("error", "Invalid credentials");
+                    res.redirect("/auth/signin");
                 } else {
-                    const userObject = foundUser.toObject();
-                    delete userObject.password; 
+                    console.log("correct");
+                    // const userObject = foundUser.toObject();
+                    // delete userObject.password; 
                     // console.log(req.session, "before defining current user");
-                    req.session.currentUser = userObject; // Stores the user in the session (data server side + a cookie is sent client side)
-                    req.flash("success", "Successfully logged in...");
+                    // req.session.currentUser = userObject; // Stores the user in the session (data server side + a cookie is sent client side)
+                    // req.flash("success", "Successfully logged in...");
                     res.redirect("/");
                 }
         }
@@ -62,8 +65,8 @@ router.post("/signin", async (req, res, next)=>{
         for (field in err.errors) {
             errorMessage += err.errors[field].message + "\n";
         } 
-        req.flash("error", errorMessage);
-        res.redirect("/signup");
+        // req.flash("error", errorMessage);
+        res.redirect("/auth/signup");
     }
 
 });
@@ -82,13 +85,13 @@ router.post("/signup", async (req, res, next)=>{
     const foundUser = await UserModel.findOne({email:newUser.email})
 
         if (foundUser) {
-            req.flash("warning", "Email already registered");
+            // req.flash("warning", "Email already registered");
             res.redirect("/signup");
         } else {
             const hashedPassword = bcrypt.hashSync(newUser.password, 10);
             newUser.password = hashedPassword;
             UserModel.create(newUser);
-            req.flash("success", "Congrats ! You are now registered !");
+            // req.flash("success", "Congrats ! You are now registered !");
             res.redirect("/signin");
         } 
 
@@ -96,7 +99,8 @@ router.post("/signup", async (req, res, next)=>{
     let errorMessage = "";
     for (field in err.errors) {
         errorMessage += err.errors[field].message + "\n";
-    } req.flash("error", errorMessage);
+    } 
+    // req.flash("error", errorMessage);
     res.redirect("/signup");
   }
 
