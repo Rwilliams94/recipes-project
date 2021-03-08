6c7591b78adafa0c186008bbb30954ed9530d99e
Rowan Williams <rmtwilliams@hotmail.co.uk>
2021-03-08 21:51:39 +0100

    signin/up enhanced

---
+++ config/session.js
@@ -0,0 +1,27 @@
+// configs/session.config.js
+
+const session = require('express-session');
+console.log(session)
+
+const MongoStore = require('connect-mongo')(session);
+
+const mongoose = require('mongoose')
+
+module.exports = app => {
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET,
+      resave: true,
+      saveUninitialized: false,
+      cookie: {
+        sameSite: 'none',
+        httpOnly: true,
+        maxAge: 60000 // 60 * 1000 ms === 1 min
+      },
+      store: new MongoStore({
+      mongooseConnection: mongoose.connection,
+      ttl: 60 * 60 * 24
+    })
+    })
+  );
+};
