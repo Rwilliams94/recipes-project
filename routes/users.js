08000f19a4572a4a73b93783ace310b9b6e78f74
adri <ad.mallie@gmail.com>
2021-03-09 18:31:05 +0100

    crud on guest, painfully made

--- routes/users.js
+++ routes/users.js
@@ -2,38 +2,17 @@ var express = require('express');
 var router = express.Router();
 const GuestModel = require("./../models/guest.model");
 
-
-router.get('/', function(req, res, next) {
-  res.render('users')
-  .then()
-  .catch(err => {
-    console.log(err);
-  });
-});
+//-------------Users-------------
 
 
-//------------GUESTS--------------
+
+//-------------------GUESTS--------------------
 
 // Guests list
 router.get("/", (req, res, next) => {
   GuestModel.find()
   .then(guests => {
-      res.render("guests/guestList", {guests});
-  })
-  .catch(err=>console.error(err));
-});
-
-
-// Create new guest
-router.get("/guest", (req, res) => {
-  res.render("guests/guestAdd");
-});
-
-router.post("/guestAdd", (req, res, next) => {
-  const { name, dietaryRequirements } = req.body;
-  GuestModel.create(req.body)
-  .then(() => {
-    res.redirect("/");
+    res.render("users", {guests});
   })
   .catch((error) => {
     next(error);
@@ -41,19 +20,55 @@ router.post("/guestAdd", (req, res, next) => {
 });
 
 
+// ----------------Create new guest------------------
+router.get("/guest", (req, res) => {
+  res.render("guests/guestAdd");
+});
 
-// Update guest
-router.get('/users/:id', (req, res) => {
-
-  GuestModel.findById(req.params.id)
-
-      .then(style => {
-          res.render('users/guestUpdate', {style});
-      })
-      .catch(error => {
-          console.log(error);
-      });
+router.post("/guestAdd", (req, res, next) => {
+  const { name, dietaryRequirements } = req.body;
+  GuestModel.create(req.body)
+  .then(guest => {
+    console.log(guest);
+    res.redirect("/users");
+  })
+  .catch((error) => {
+    next(error);
   });
+});
 
+
+// ---------------Update guest-------------------
+router.get("/guestUpdate/:id", (req, res, next) => {
+  GuestModel.findById(req.params.id)
+  .then((guest) => {
+    res.render("guests/guestUpdate", {guest} );
+  })
+  .catch((dbError) => {
+    next(dbError);
+  });
+});
+
+    
+router.post("/guestUpdate/:id", (req, res, next) => {
+  GuestModel.findByIdAndUpdate(req.params.id, req.body, {new:true})
+  .then(() => {
+    res.redirect("/users");
+  })
+  .catch((error)=> {
+    next(error);
+  });
+});
+
+//----------------Delete guest---------------------
+router.get('/delete/:id', (req, res, next) => {
+  GuestModel.findByIdAndDelete(req.params.id)
+  .then(()=> {
+    res.redirect("/users");
+  })
+  .catch((error) => {
+    next(error);
+  });
+});
 
 module.exports = router;
