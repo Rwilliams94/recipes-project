b98bb66d208edd7ab9e1399729f162015436a9c2
adri <ad.mallie@gmail.com>
2021-03-11 16:18:18 +0100

    mongo env

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://teamRecipe:WrzCAfkcybSzzdB1@cluster0.0bmpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+mongoose.connect("process.env.MONGO_URI", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
