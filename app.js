307b37d8a77aa401e35aa39a0a5b75eff7cb720e
adri <ad.mallie@gmail.com>
2021-03-11 17:09:24 +0100

    error comment

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  console.log("custom error : ",err);
+  // console.log("custom error : ",err);
   res.render("error");
 });
 
