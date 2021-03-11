25d85f221703db61921aae7823bff940a43559f4
adri <ad.mallie@gmail.com>
2021-03-11 16:12:15 +0100

    mongo try

--- config/mongodb.js
+++ config/mongodb.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://rmtwilliams94:Ironhack2021@cluster0.0bmpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://teamRecipe:WrzCAfkcybSzzdB1@cluster0.0bmpa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
